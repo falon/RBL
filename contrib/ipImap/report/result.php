5c10b775e9f9e5e251abb268ed195b4a71068b0f
root <root@tst-msg02.csi.it>
2017-02-07 12:12:53 +0100

    style fix

--- contrib/ipImap/report/result.php
+++ contrib/ipImap/report/result.php
@@ -12,8 +12,8 @@ foreach($dir as $file)
 		echo '<li><a href="#'.$modalDiv.'">'.str_replace("-$theDate.html",'',basename($file)).'</a></li>';
 #                echo '<li><pre><a href="'.basename($file).'" title="'.str_replace("-$theDate.html",'',basename($file))." of $theDate".'" onClick="Modalbox.show(this.href, {title: this.title, height: 600}); return false;">'.str_replace("-$theDate.html",'',basename($file)).'</a></pre></li>';
 		print <<<MODAL
-<div id="$modalDiv" class="modalDialog">
-	<div>
+<div id="$modalDiv" class="overlay">
+	<div class="popup">
 		<a href="#close" title="Close" class="close">X</a>
 MODAL;
 		readfile(basename($file));
