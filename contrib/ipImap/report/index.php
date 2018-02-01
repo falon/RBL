43fe33c55195f51253bfe48a1f03ccf0a691d08f
Marco Favero <marco.favero@csi.it>
2018-02-01 12:24:50 +0100

    remove calendar JS. Please, use HTML5 browser with input date type.

--- contrib/ipImap/report/index.php
+++ contrib/ipImap/report/index.php
@@ -11,12 +11,10 @@ window.defaultStatus='IRBLYou! -- A great extension of IQueueYou!';
 <script  src="/include/ajaxsbmt.js" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript" charset="utf-8"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js?load=effects" type="text/javascript" charset="utf-8"></script>
-<script language="javascript" src="calendar/calendar.js"></script>
-<link href="calendar/calendar.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <h1>Learn Tool Status</h1>
-<form name="formSpam" id="formSpam" onSubmit="xmlhttpPost('result.php', 'formSpam', 'Risultato', '<img src=\'/include/pleasewait.gif\'>'); return false;" enctype="text/plain" method="post" target="_self" class="left">
+<form name="formSpam" id="formSpam" onSubmit="xmlhttpPost('result.php', 'formSpam', 'content', '<img src=\'/include/pleasewait.gif\'>'); return false;" enctype="text/plain" method="post" target="_self" class="left">
 
 <?php
 $reports = array();
@@ -24,29 +22,14 @@ foreach (glob("*.{htm,html}", GLOB_BRACE) as $filename) {
     $reports[$filename] = filemtime($filename);
 }
 arsort($reports);
-$newest = array_shift($reports);
-$oldest = array_pop($reports);
-
-require_once('calendar/classes/tc_calendar.php');
-/* Write Calendar */
-	  $myCalendar = new tc_calendar("calSpam");
-	  $myCalendar->setIcon("calendar/images/iconCalendar.gif");
-	  $myCalendar->setDate(date('d',$newest), date('m',$newest), date('Y',$newest));
-	  $myCalendar->setPath("calendar/");
-	  $myCalendar->setYearInterval(date('Y',$oldest), date('Y', $newest));
-	  $myCalendar->dateAllow(date('Y-m-d', $oldest), date('Y-m-d', $newest));
-	  $myCalendar->setDateFormat('j F Y');
-	  $myCalendar->startDate(1);
-	  $myCalendar->setAlignment('left', 'bottom');
-#	  $myCalendar->setSpecificDate(array("2011-06-01"), 0, '');
-#	  $myCalendar->autoSubmit(true, 'formSpam', 'result.php'); 
-	  $myCalendar->setTheme('theme1');
-	  $myCalendar->writeScript();
-/*********************************/
+$newest = new DateTime('@'.array_shift($reports));
+$oldest = new DateTime('@'.array_pop($reports));
+printf('<input type="date" name="calSpam" min="%s" max="%s" value="%s">',
+	$oldest->format('Y-m-d'), $newest->format('Y-m-d'), $newest->format('Y-m-d'));
 ?>
-<input type="submit" name="Check Reports" value="Submit" />
+<input type="submit" name="Check Reports" value="Engage" />
 </form>
-<div class="left" id="Risultato">
+<div class="right" id="content">
 </div>
 </body>
 </html>
